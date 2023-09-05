ee6cad530aaf278ef8e8c3451d6626e41d939c6d
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-09-05 15:33:42 +0100

    like button added, feedback local

---
+++ src/PatchVoteCount.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default function PatchVoteCount(article_id){
+axios
+.patch(`https://laura-news.onrender.com/api/articles/${article_id}`,{
+     inc_votes: 1 
+})
+}
--- src/SingleArticle.jsx
+++ src/SingleArticle.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import PatchVoteCount from "./PatchVoteCount";
 
 export default function SingleArticle() {
   const { article_id } = useParams();
 
   const [individualArticle, setIndividualArticle] = useState({});
 
-  const [currentLikeCount, setCurrentLikeCount] = useState(0);
+  const [likeCount, SetLikeCount ] = useState(0);
+
 
   useEffect(() => {
     axios
@@ -15,12 +17,16 @@ export default function SingleArticle() {
       .then(({ data }) => {
         const { article } = data;
         setIndividualArticle(article);
-        setCurrentLikeCount(article.votes);
+        SetLikeCount(article.votes);
       });
   }, []);
 
   function handleLikeClick() {
-    const localCount = currentLikeCount++;
+    SetLikeCount((likeCount)=>{
+      likeCount++
+    })
+    console.log(likeCount)
+    PatchVoteCount(article_id)
   }
 
   return (
@@ -42,10 +48,10 @@ export default function SingleArticle() {
         <main className="individualArticleBody">{individualArticle.body}</main>
       </section>
       <footer className="individualArticleFooter">
-        <button className="likeButton">
+        <button className="likeButton" onClick={handleLikeClick}>
           <p>Like</p>
-          <p className="badge" onClick={handleLikeClick}>
-            {individualArticle.votes}
+          <p className="badge">
+            {likeCount}
           </p>
         </button>
         <Link to={`/articles/${article_id}/comments`}className="CommentLink" >View Comments</Link>
