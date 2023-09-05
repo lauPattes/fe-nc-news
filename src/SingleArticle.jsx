31c055dcc97717f3e4f304d36268894170792f14
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-09-05 09:19:13 +0100

    header and main of individualArticle page rendered

--- src/SingleArticle.jsx
+++ src/SingleArticle.jsx
@@ -1,35 +1,41 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
+export default function SingleArticle() {
+  const { article_id } = useParams();
 
-export default function SingleArticle(){
-    const {article_id} = useParams()
+  const [individualArticle, setIndividualArticle] = useState({});
 
-    const [individualArticle, setIndividualArticle] = useState({})
-
-    
-    useEffect(()=>{
-       axios
-       .get(`https://laura-news.onrender.com/api/articles/${article_id}`) 
-       .then(({data})=>{
-        const {article} = data 
+  useEffect(() => {
+    axios
+      .get(`https://laura-news.onrender.com/api/articles/${article_id}`)
+      .then(({ data }) => {
+        const { article } = data;
         console.log(article);
-        setIndividualArticle(article)
-       })
-    },[])
+        setIndividualArticle(article);
+      });
+  }, []);
 
-    return (
-        <>
-        <header>
-            <h1 className="articleTitle">{individualArticle.title}</h1>
-            <h2 className="articleAuthor">{individualArticle.author}</h2>
-            <Link to="/">Home</Link>
-            <button>Back to topics</button>
-        </header>
-        <section>
-            
-        </section>
-        </>
-    )
+  return (
+    <>
+      <header>
+        <h1 className="articleTitle">{individualArticle.title}</h1>
+        <h2 className="articleAuthor">{individualArticle.author}</h2>
+        <Link to="/">Home</Link>
+        <button>Back to {individualArticle.topic}</button>
+      </header>
+      <main className="individualArticleSection">
+        <img
+          className="individualArticleImage"
+          src={individualArticle.article_img_url}
+          alt={individualArticle.title}
+        />
+        <body className="individualArticleBody">{individualArticle.body}</body>
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  );
 }
