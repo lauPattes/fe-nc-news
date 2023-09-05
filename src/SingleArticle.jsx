23a13e8b22ddec9315cbff9c3caaea35ba72fe21
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-09-05 08:56:54 +0100

    working on individual article page

--- src/SingleArticle.jsx
+++ src/SingleArticle.jsx
@@ -1,6 +1,35 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+
+
 export default function SingleArticle(){
+    const {article_id} = useParams()
+
+    const [individualArticle, setIndividualArticle] = useState({})
+
+    
+    useEffect(()=>{
+       axios
+       .get(`https://laura-news.onrender.com/api/articles/${article_id}`) 
+       .then(({data})=>{
+        const {article} = data 
+        console.log(article);
+        setIndividualArticle(article)
+       })
+    },[])
+
     return (
         <>
+        <header>
+            <h1 className="articleTitle">{individualArticle.title}</h1>
+            <h2 className="articleAuthor">{individualArticle.author}</h2>
+            <Link to="/">Home</Link>
+            <button>Back to topics</button>
+        </header>
+        <section>
+            
+        </section>
         </>
     )
 }
