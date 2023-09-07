7cf445300dc3c011035e7c8177693f29ab4f3a83
Laura Patterson <laura.patterson53@hotmail.co.uk>
2023-09-07 16:48:46 +0100

    having trouble with sorting

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,16 +6,33 @@ import axios from "axios";
 export default function Home() {
   const [ArticleArray, setArticleArray] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const[sort, setSort] = useState(undefined)
+  const[upOrDown, setUpOrDown] = useState(undefined)
+
 
   useEffect(() => {
     axios
-      .get("https://laura-news.onrender.com/api/articles")
+      .get("https://laura-news.onrender.com/api/articles",{
+        sort_by: sort,
+        order: upOrDown
+      })
       .then(({ data }) => {
         const { articles } = data;
         setArticleArray(articles);
         setIsLoading(false)
       });
-  }, []);
+  }, [sort, upOrDown]);
+
+  function sortByDateAsc(){
+    setSort("created_at")
+    setUpOrDown("asc")
+  }
+  
+  function sortByDateDesc(){
+    setSort("created_at")
+    setUpOrDown("desc")
+  }
+
 
   return (
     <>
@@ -27,10 +44,10 @@ export default function Home() {
         </nav>
         <section className="sortButtons">
           Order By:
-          <button className="dates">
+          <button className="dates" onClick={sortByDateAsc}>
             Date Asc
           </button>
-          <button className="dates">
+          <button className="dates" onClick={sortByDateDesc}>
             Date Desc
           </button>
           <button>
